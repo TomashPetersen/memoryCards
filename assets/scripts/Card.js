26080e97c17e3613f047a63ea0befc421b75d991
Denis Volkov <d.g.volkovbz@gmail.com>
2022-03-22 22:21:21 +0400

    Add level win and restart feature

--- assets/scripts/Card.js
+++ assets/scripts/Card.js
@@ -1,11 +1,11 @@
 class Card extends Phaser.GameObjects.Sprite {
-    constructor(scene, value, position) {
-        super(scene, position.x, position.y, 'card');
+    constructor(scene, value) {
+        super(scene, 0, 0, 'card');
         this.scene = scene;
         this.value = value;
         this.setOrigin(0, 0);
         this.scene.add.existing(this);
-        this.opend = false;
+        this.opened = false;
 
         this.setInteractive();
         // this.on('pointerdown', this.open, this);
@@ -13,11 +13,11 @@ class Card extends Phaser.GameObjects.Sprite {
 
     open() {
         this.setTexture("card" + this.value);
-        this.opend = true;
+        this.opened = true;
     }
 
     close() {
         this.setTexture("card");
-        this.opend = false;
+        this.opened = false;
     }
 }
--- assets/scripts/GameScene.js
+++ assets/scripts/GameScene.js
@@ -4,61 +4,73 @@ class GameScene extends Phaser.Scene {
     }
     preload() {
         this.load.image('bg', 'assets/sprites/background.png');
-        this.load.image('card', 'assets/sprites/card.png');    
-        this.load.image('card1', 'assets/sprites/card1.png');    
-        this.load.image('card2', 'assets/sprites/card2.png');    
-        this.load.image('card3', 'assets/sprites/card3.png');    
-        this.load.image('card4', 'assets/sprites/card4.png');    
-        this.load.image('card5', 'assets/sprites/card5.png');    
+        this.load.image('card', 'assets/sprites/card.png');
+        this.load.image('card1', 'assets/sprites/card1.png');
+        this.load.image('card2', 'assets/sprites/card2.png');
+        this.load.image('card3', 'assets/sprites/card3.png');
+        this.load.image('card4', 'assets/sprites/card4.png');
+        this.load.image('card5', 'assets/sprites/card5.png');
     }
-
     create() {
         this.createBackground();
-
         this.createCards();
-
-        this.openedCard = null;
+        this.start();
     }
+    start() {
+        this.openedCard = null;
+        this.openedCardsCount = 0;
+        this.initCards();
+    }
+    initCards() {
+        let positions = this.getCardsPositions();
 
+        this.cards.forEach(card => {
+            let position = positions.pop();
+            card.close();
+            card.setPosition(position.x, position.y);
+        });
+    }
     createBackground() {
         this.add.sprite(0, 0, 'bg').setOrigin(0, 0);
     }
     createCards() {
         this.cards = [];
-        let positions = this.getCardsPositions();
-        Phaser.Utils.Array.Shuffle(positions); // сортируем массив в случайном порядке с помощью метода библиотеки Phaser
 
         for (let value of config.cards) {
             for (let i = 0; i < 2; i++) {
-                this.cards.push(new Card(this, value, positions.pop()));
+                this.cards.push(new Card(this, value));
             }
         }
 
         this.input.on("gameobjectdown", this.onCardClicked, this);
     }
-
     onCardClicked(pointer, card) {
         if (card.opened) {
             return false;
         }
+
         if (this.openedCard) {
-            //уже есть открытая карта
+            // уже есть открытая карта
             if (this.openedCard.value === card.value) {
-                //картинки равны - запомнить
+                // картинки равны - запомнить
                 this.openedCard = null;
+                ++this.openedCardsCount;
             } else {
-                // картинки разные - скрыть предыдущую карту
+                // картинки разные - скрыть прошлую
                 this.openedCard.close();
                 this.openedCard = card;
             }
         } else {
-            //еще нет открытой карты
+            // еще нет открытой карта
             this.openedCard = card;
         }
 
         card.open();
-    }
 
+        if (this.openedCardsCount === this.cards.length / 2) {
+            this.start();
+        }
+    }
     getCardsPositions() {
         let positions = [];
         let cardTexture = this.textures.get('card').getSourceImage();
@@ -66,16 +78,16 @@ class GameScene extends Phaser.Scene {
         let cardHeight = cardTexture.height + 4;
         let offsetX = (this.sys.game.config.width - cardWidth * config.cols) / 2;
         let offsetY = (this.sys.game.config.height - cardHeight * config.rows) / 2;
-
+    
         for (let row = 0; row < config.rows; row++) {
             for (let col = 0; col < config.cols; col++) {
                 positions.push({
                     x: offsetX + col * cardWidth,
-                    y: offsetY + row * cardHeight
+                    y: offsetY + row * cardHeight,
                 });
             }
         }
 
-        return positions;
+        return Phaser.Utils.Array.Shuffle(positions);
     }
 }
